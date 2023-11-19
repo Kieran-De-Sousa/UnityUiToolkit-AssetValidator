17a1d58fea29ed6dc3210a950b10764719f579c9
Kieran De Sousa <kieran2.desousa@live.uwe.ac.uk>
2023-11-19 17:46:00 +0000

    Add: Open Tool on Asset Import
    + Improved loading of Asset Image with IEnumerator method.

---
+++ 19007744-UiToolkit/Assets/Editor/s_AssetPostProcess.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+public class s_AssetPostProcess : AssetPostprocessor
+{
+    // This method will be called whenever assets are imported into the project
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string assetPath in importedAssets)
+        {
+            // Load the imported asset
+            Object importedAsset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+
+            if (s_ValidationMethods.IsValidAssetType(importedAsset))
+            {
+                // Select the imported asset in the Editor
+                Selection.activeObject = importedAsset;
+
+                // Open Asset Validator window
+                s_AssetValidator.ShowWindow();
+            }
+        }
+    }
+}
--- 19007744-UiToolkit/Assets/Scripts/UI Toolkit/s_AssetValidator.cs
+++ 19007744-UiToolkit/Assets/Scripts/UI Toolkit/s_AssetValidator.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using Unity.EditorCoroutines.Editor;
+
 using UnityEngine;
 using UnityEditor;
+
 using UnityEngine.UIElements;
+
+using Object = UnityEngine.Object;
 
 public class s_AssetValidator : EditorWindow
 {
@@ -10,10 +17,12 @@ public class s_AssetValidator : EditorWindow
     VisualElement m_selectedAssetsContainer;
     VisualElement m_selectedAssetsImage;
 
+    private const float ASSET_LOAD_WAIT = 0.1f; // EditorWaitForSeconds value.
+
     /// <summary>
     /// String constants of UI Toolkit
     /// </summary>
-    private const string MENU_ITEM = "Tools/Asset Validator";
+    private const string MENU_ITEM = "Tools/Asset Validator/Asset Validator";
     private const string WINDOW_NAME = "Asset Validator";
 
     private const string PATH_UIDOCUMENT = "Assets/UI/UI Documents/uxml_AssetValidator.uxml";
@@ -39,8 +48,8 @@ public class s_AssetValidator : EditorWindow
         visualTree.CloneTree(root);
 
         m_selectedAssetsContainer = root.Q<VisualElement>(VE_ASSETCONTAINER);
-        //m_selectedAssetsImage = m_selectedAssetsContainer.Q<VisualElement>(VE_ASSETPREVIEW);
 
+        //m_selectedAssetsImage = m_selectedAssetsContainer.Q<VisualElement>(VE_ASSETPREVIEW);
 
         Button validateButton = root.Q<Button>(BUTTON_VALIDATE);
         validateButton.clicked += ValidateSelection;
@@ -60,18 +69,47 @@ public class s_AssetValidator : EditorWindow
         {
             if (s_ValidationMethods.IsValidAssetType(asset))
             {
-                // Display a label of the assets name.
-                Label assetLabel = new Label(asset.name);
-                m_selectedAssetsContainer.Add(assetLabel);
-            
-                // Display the selected (or most recently selected) asset's Unity preview image.
-                Texture2D texture = AssetPreview.GetAssetPreview(asset);
-                m_selectedAssetsContainer.style.backgroundImage = texture;
-                
+                // Sets Asset name in tool window.
+                SetPreviewLabel(asset);
+
+                // Sets Asset preview GUI image in tool window.
+                EditorCoroutineUtility.StartCoroutineOwnerless(SetPreviewImage(asset));
+
                 m_selectedAssets.Add(asset);
             }
+        }
+    }
 
-            // TODO: OTHER ASSET TYPES
+    /// <summary>
+    /// Sets the preview name of the asset as the preview name in the <c>window</c> container.
+    /// </summary>
+    /// <param name="asset">Asset to have preview name shown.</param>
+    private void SetPreviewLabel(Object asset)
+    {
+        // Display a label of the assets name.
+        Label assetLabel = new Label(asset.name);
+        m_selectedAssetsContainer.Add(assetLabel);
+    }
+
+    /// <summary>
+    /// Sets the preview image of the asset as the preview image in the <c>window</c> container.
+    /// </summary>
+    /// <param name="asset">Asset to have preview image shown.</param>
+    /// <returns><c>backgroundImage</c> for asset preview.</returns>
+    IEnumerator SetPreviewImage(Object asset)
+    {
+        Texture2D texture = AssetPreview.GetAssetPreview(asset);
+
+        // Check if a valid texture preview is available for the asset
+        if (texture != null)
+        {
+            m_selectedAssetsContainer.style.backgroundImage = texture;
+        }
+        else
+        {
+            // If asset preview is not available, wait for ASSET_LOAD_WAIT amount of time before attempting
+            // to set the asset preview again. Repeat until successful.
+            yield return new EditorWaitForSeconds(ASSET_LOAD_WAIT);
         }
     }
 
